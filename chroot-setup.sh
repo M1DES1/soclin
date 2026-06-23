a847577c5591ce9421f12116ca2fb6e0c7a8deca
M1DES1 <141944173+M1DES1@users.noreply.github.com>
2026-06-23 18:31:45 +0200

    Add files via upload

--- chroot-setup.sh
+++ chroot-setup.sh
@@ -2,7 +2,6 @@
 set -ex
 
 # Ten skrypt działa w środowisku root chroot
-
 export DEBIAN_FRONTEND=noninteractive
 
 # Ustawienie repozytoriów
@@ -16,78 +15,31 @@ apt-get update
 apt-get upgrade -y
 
 # 1. Instalacja podstawowych pakietów środowiska i kernela
-apt-get install -y linux-image-generic linux-headers-generic initramfs-tools casper dbus systemd-sysv sudo locales nano wget curl git
+apt-get install -y linux-image-generic linux-headers-generic initramfs-tools casper sudo \
+    locales nano wget curl git dbus systemd-sysv network-manager
 
-# Wygeneruj locale
 locale-gen en_US.UTF-8
 update-locale LANG=en_US.UTF-8
 
-# 2. Instalacja Wayland, ułatwień i menedżera logowania, terminala (kitty), file menedżera (thunar)
-apt-get install -y xwayland libwayland-dev wayland-protocols \
-    kitty thunar thunar-archive-plugin \
-    polkit-kde-agent-1 qtwayland5 qt6-wayland \
-    mesa-utils vulkan-tools sddm \
-    pavucontrol network-manager network-manager-gnome
+# 2. Instalacja absolutnego minimum z GUI, żeby pokazać okno instalacji po zbotowaniu
+# Użyjemy Openbox i Kitty. Zenity pomoże pokazać okienko powitalne.
+apt-get install -y xserver-xorg xinit openbox kitty zenity sddm \
+    pavucontrol network-manager-gnome
 
-# 3. Instalacja narzędzi do zbudowania Hyprland (bez domyślnego cmake, bo jest za stare)
-apt-get install -y meson cpio build-essential g++ pkg-config \
-    libdrm-dev libegl-dev libegl1-mesa-dev libgles2-mesa-dev libgl1-mesa-dev \
-    libgbm-dev libinput-dev libudev-dev libwayland-dev libxcursor-dev \
-    libxkbcommon-dev libseat-dev libxcb1-dev libxcb-render0-dev \
-    libxcb-shape0-dev libxcb-xfixes0-dev libvulkan-dev \
-    glslang-tools glslang-dev libdisplay-info-dev \
-    libpixman-1-dev uuid-dev libffi-dev ninja-build \
-    libxcb-composite0-dev libxcb-present-dev libxcb-dri3-dev libxcb-res0-dev \
-    libx11-xcb-dev hwdata jq
-
-# Aktualizacja CMake do wersji >= 3.30 (wymagane przez najnowsze Hyprland)
-wget https://github.com/Kitware/CMake/releases/download/v3.31.4/cmake-3.31.4-linux-x86_64.tar.gz
-tar -zxvf cmake-3.31.4-linux-x86_64.tar.gz -C /usr/local --strip-components=1
-rm cmake-3.31.4-linux-x86_64.tar.gz
-
-# 4. Kompilacja Hyprland (zgodnie z życzeniem ze źródeł)
-cd /opt
-if [ ! -d "Hyprland" ]; then
-    git clone --recursive https://github.com/hyprwm/Hyprland
-    cd Hyprland
-    make all
-    make install
-fi
-
-# 5. Instalacja i konfiguracja WINE (uruchamianie .exe)
-dpkg --add-architecture i386
-apt-get update
-apt-get install -y wine64 wine32 wine-binfmt
-
-# Aktualizuj konfigurację mime żeby Wine w Thunar było po dwukliku
-mkdir -p /etc/skel/.config
-cp /root/config/mimeapps.list /etc/skel/.config/
-# Automatyczny skrypt do Wine (czasem przydatny zamiast domyślnego)
-cat << 'EOF' > /usr/local/bin/run-exe.sh
-#!/bin/bash
-wine "$@"
-EOF
-chmod +x /usr/local/bin/run-exe.sh
-
-# 6. Kopiowanie Themu Hyprland
+# Kopiowanie motywu Hyprland do użytku po instalacji
 mkdir -p /etc/skel/.config/hypr
 cp /root/config/hyprland.conf /etc/skel/.config/hypr/hyprland.conf
+cp /root/config/mimeapps.list /etc/skel/.config/
 
-# Do "windows 11 look" pobieramy GTK theme z githuba (Win11 theme od vinceliuice)
-apt-get install -y sassc optipng inkscape libglib2.0-dev xmlstarlet
-cd /opt
-git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git win11-theme
-cd win11-theme
-./install.sh -c Light -t all -N glassy
+# Iniekcja Skryptu Instalacyjnego do autostartu
+cp /root/config/winux-setup.sh /usr/local/bin/winux-setup.sh
+chmod +x /usr/local/bin/winux-setup.sh
 
-# Domyślny theme do ~/.config/gtk-3.0/settings.ini w /etc/skel
-mkdir -p /etc/skel/.config/gtk-3.0
-cat <<EOF > /etc/skel/.config/gtk-3.0/settings.ini
-[Settings]
-gtk-theme-name=WhiteSur-Light
-gtk-icon-theme-name=WhiteSur
-gtk-font-name=Sans 11
-gtk-application-prefer-dark-theme=0
+# Openbox Autostart (odpala okienko przy pierwszym włączeniu live)
+mkdir -p /etc/skel/.config/openbox
+cat <<EOF > /etc/skel/.config/openbox/autostart
+# Odpal instalator w terminalu, żeby użytkownik widział postęp
+kitty -e sudo bash /usr/local/bin/winux-setup.sh &
 EOF
 
 # 7. Utworzenie usera Live
@@ -95,18 +47,17 @@ useradd -m -s /bin/bash -G sudo,video,audio,plugdev,netdev live
 echo "live:live" | chpasswd
 echo "live ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
-# SDDM Auto-login
+# SDDM Auto-login do Openbox
 mkdir -p /etc/sddm.conf.d
 cat <<EOF > /etc/sddm.conf.d/autologin.conf
 [Autologin]
 User=live
-Session=hyprland
+Session=openbox
 EOF
 
-# Czyszczenie na koniec
+# Czyszczenie na koniec ISO buildu
 apt-get clean
 rm -rf /var/lib/apt/lists/*
 rm -rf /tmp/*
 
-# Wygeneruj ostateczny Initramfs
 update-initramfs -u
---
+++ config/winux-setup.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+zenity --info --title="Winux-OS Installer" --text="Rozpoczynam instalację Hyprland, motywów Windows 11 i środowiska Wine. \n\nProszę czekać, może to potrwać kilkanaście minut na połączenie z internetem i pobieranie paczek." --width=400
+
+# Włączenie repozytoriów jeżeli system działa z RAMu
+apt-get update
+
+# Instalacja pełnych zależności Wayland 
+apt-get install -y meson cpio build-essential g++ pkg-config \
+    libdrm-dev libegl-dev libegl1-mesa-dev libgles2-mesa-dev libgl1-mesa-dev \
+    libgbm-dev libinput-dev libudev-dev libwayland-dev libxcursor-dev \
+    libxkbcommon-dev libseat-dev libxcb1-dev libxcb-render0-dev \
+    libxcb-shape0-dev libxcb-xfixes0-dev libvulkan-dev \
+    glslang-tools glslang-dev libglslang-dev libdisplay-info-dev \
+    libhwdata-dev libpixman-1-dev uuid-dev libffi-dev ninja-build \
+    libxcb-composite0-dev libxcb-present-dev libxcb-dri3-dev libxcb-res0-dev \
+    libx11-xcb-dev hwdata jq thunar thunar-archive-plugin \
+    wine64 wine32 wine-binfmt xdg-desktop-portal-wlr qtwayland5 qt6-wayland xwayland
+
+# Ręczna instalacja wymaganego CMake 3.31
+cd /tmp
+wget https://github.com/Kitware/CMake/releases/download/v3.31.4/cmake-3.31.4-linux-x86_64.tar.gz
+tar -zxvf cmake-3.31.4-linux-x86_64.tar.gz -C /usr/local --strip-components=1
+rm cmake-3.31.4-linux-x86_64.tar.gz
+
+# Pobieranie i kompilacja Hyprland
+cd /opt
+if [ ! -d "Hyprland" ]; then
+    git clone --recursive https://github.com/hyprwm/Hyprland
+    cd Hyprland
+    make all
+    make install
+fi
+
+# Instalacja Win11 Theme
+apt-get install -y sassc optipng inkscape libglib2.0-dev xmlstarlet
+cd /opt
+if [ ! -d "win11-theme" ]; then
+    git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git win11-theme
+    cd win11-theme
+    ./install.sh -c Light -t all -N glassy
+fi
+
+# Zmiana domyślnej sesji w autologin na hyprland
+cat <<EOF > /etc/sddm.conf.d/autologin.conf
+[Autologin]
+User=live
+Session=hyprland
+EOF
+
+mkdir -p /home/live/.config/gtk-3.0
+cat <<EOF > /home/live/.config/gtk-3.0/settings.ini
+[Settings]
+gtk-theme-name=WhiteSur-Light
+gtk-icon-theme-name=WhiteSur
+gtk-font-name=Sans 11
+gtk-application-prefer-dark-theme=0
+EOF
+chown -R live:live /home/live/.config/
+
+# Konfiguracja otwierania i integracji .exe dla Wine
+cp /home/live/.config/mimeapps.list /etc/skel/.config/ 2>/dev/null || true
+mkdir -p /home/live/.local/share/applications
+# Desktop app do otworzenia wine
+cat <<EOF > /home/live/.local/share/applications/wine.desktop
+[Desktop Entry]
+Name=Wine Windows Program Loader
+Exec=wine start /unix %f
+Type=Application
+StartupNotify=true
+Path=/tmp
+Icon=wine
+Categories=X-Wine;
+MimeType=application/x-ms-dos-executable;application/x-msi;application/x-ms-shortcut;
+NoDisplay=true
+EOF
+chown -R live:live /home/live/.local/
+
+zenity --info --title="Instalacja Zakończona" --text="System Winux-OS z Hyprland jest gotowy. \n\nInstalator uruchomi system ponownie by odpalić środowisko Winux." --width=400
+
+# Zrestartuj aby wbiło menedżera logowania bezposródnio do Hyprland
+reboot
