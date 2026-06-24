9b281e03825d5e51354116c90352795cbc4dad1f
M1DES1 <141944173+M1DES1@users.noreply.github.com>
2026-06-24 17:47:06 +0200

    Add files via upload

--- build-iso.sh
+++ build-iso.sh
@@ -1,79 +1,46 @@
 #!/bin/bash
-set -ex
+set -euo pipefail
 
-# Zmienne środowiskowe
+# Budujemy proste ISO, które bootuje bezpośrednio do oficjalnego
+# graficznego Debian Installer, bez sesji live i bez Calamares.
 export DEBIAN_FRONTEND=noninteractive
-export ISO_NAME="soclin"
+export ISO_NAME="soclin-debian-installer"
 export WORKDIR="$(pwd)/iso_build"
-export ROOTFS="$WORKDIR/chroot"
 export ISO_DIR="$WORKDIR/iso"
-export DATE=$(date +%Y%m%d)
+export DATE
+DATE="$(date +%Y%m%d)"
 
-# 1. Przygotowanie folderów
+export DEBIAN_SUITE="${DEBIAN_SUITE:-stable}"
+export DEBIAN_MIRROR="${DEBIAN_MIRROR:-https://deb.debian.org/debian}"
+export INSTALLER_BASE="${DEBIAN_MIRROR}/dists/${DEBIAN_SUITE}/main/installer-amd64/current/images/netboot/gtk/debian-installer/amd64"
+
 rm -rf "$WORKDIR"
-mkdir -p "$ROOTFS"
-mkdir -p "$ISO_DIR/casper"
-mkdir -p "$ISO_DIR/boot/grub"
+mkdir -p "$ISO_DIR/boot/grub" "$ISO_DIR/install.amd"
 
-# 2. Budowa bazowego systemu (Ubuntu Noble 24.04)
-echo "=== Instalowanie bazowego systemu Noble ==="
-debootstrap --arch=amd64 noble "$ROOTFS" http://archive.ubuntu.com/ubuntu/
+echo "=== Pobieranie Debian Installer (${DEBIAN_SUITE}) ==="
+wget -qO "$ISO_DIR/install.amd/linux" "${INSTALLER_BASE}/linux"
+wget -qO "$ISO_DIR/install.amd/initrd.gz" "${INSTALLER_BASE}/initrd.gz"
 
-# Zamontowanie systemów plików wewnątrz chroot
-mount --bind /dev "$ROOTFS/dev"
-mount -t proc proc "$ROOTFS/proc"
-mount -t sysfs sysfs "$ROOTFS/sys"
-mount -t devpts devpts "$ROOTFS/dev/pts"
+cat <<'EOF' > "$ISO_DIR/boot/grub/grub.cfg"
+set default=0
+set timeout=0
+set timeout_style=hidden
 
-# 3. Kopiowanie skryptu instalacyjnego i konfiguracji do chroot
-cp chroot-setup.sh "$ROOTFS/root/"
-chmod +x "$ROOTFS/root/chroot-setup.sh"
-cp -r config "$ROOTFS/root/"
-
-# Skopiowanie repo soclin do ewentualnych źródeł (opcjonalne)
-# cp -r . "$ROOTFS/root/soclin-repo"
-
-echo "=== Uruchamianie chroot-setup.sh ==="
-chroot "$ROOTFS" /bin/bash /root/chroot-setup.sh
-
-# Sprzątanie wewnątrz
-rm -rf "$ROOTFS/root/chroot-setup.sh" "$ROOTFS/root/config"
-
-# Odmontowanie
-umount "$ROOTFS/dev/pts"
-umount "$ROOTFS/sys"
-umount "$ROOTFS/proc"
-umount "$ROOTFS/dev"
-
-# 4. Kernel i Initrd
-echo "=== Przygotowanie bootowania ==="
-cp "$ROOTFS/boot/vmlinuz-"* "$ISO_DIR/boot/vmlinuz" || cp "$ROOTFS/boot/vmlinuz" "$ISO_DIR/boot/vmlinuz"
-cp "$ROOTFS/boot/initrd.img-"* "$ISO_DIR/boot/initrd.img" || cp "$ROOTFS/boot/initrd.img" "$ISO_DIR/boot/initrd.img"
-
-# 5. Generowanie SquashFS
-echo "=== Kompresja do SquashFS ==="
-mksquashfs "$ROOTFS" "$ISO_DIR/casper/filesystem.squashfs" -comp xz -wildcards -e boot/vmlinuz* boot/initrd.img*
-
-sleep 2
-printf $(du -sx --block-size=1 "$ROOTFS" | cut -f1) > "$ISO_DIR/casper/filesystem.size"
-
-# 6. GRUB config
-cat <<EOF > "$ISO_DIR/boot/grub/grub.cfg"
-set default="0"
-set timeout=5
-menuentry "soclin" {
-    linux /boot/vmlinuz boot=casper username=live hostname=soclin quiet splash ---
-    initrd /boot/initrd.img
+menuentry "soclin Debian Installer" {
+    linux /install.amd/linux auto=true priority=medium language=pl country=PL locale=pl_PL.UTF-8 keyboard-configuration/xkb-keymap=pl hostname=soclin domain=local quiet ---
+    initrd /install.amd/initrd.gz
 }
-menuentry "soclin (Safe Graphics)" {
-    linux /boot/vmlinuz boot=casper nomodeset username=live hostname=soclin quiet splash ---
-    initrd /boot/initrd.img
+
+menuentry "soclin Debian Installer (safe graphics)" {
+    linux /install.amd/linux auto=true priority=medium language=pl country=PL locale=pl_PL.UTF-8 keyboard-configuration/xkb-keymap=pl hostname=soclin domain=local nomodeset quiet ---
+    initrd /install.amd/initrd.gz
 }
 EOF
 
-# 7. Budowanie pliku ISO za pomocą xorriso-dd-target / grub-mkrescue
-echo "=== Generowanie pliku ISO ==="
+echo "=== Generowanie sum kontrolnych ==="
 (cd "$ISO_DIR" && find . -type f -not -name md5sum.txt -print0 | xargs -0 md5sum > md5sum.txt)
+
+echo "=== Budowanie ISO ==="
 grub-mkrescue -o "${ISO_NAME}-${DATE}.iso" "$ISO_DIR"
 
 echo "=== Gotowe: ${ISO_NAME}-${DATE}.iso ==="
